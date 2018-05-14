38fb92245dde64662ae7c05641e8eb36d9071bd4
Alisson Youssf <youssfbr@gmail.com>
2018-05-14 15:14:42 -0300

    Função que retorna dados.

--- static/js/main.js
+++ static/js/main.js
@@ -17,11 +17,31 @@ function cadastraVeiculo(e) {
 		carros.push(carro);
 		localStorage.setItem('patio2', JSON.stringify(carros));
 	} else {
-		var carros = JSON.parse(localStorage.getItem('patio'));
+		var carros = JSON.parse(localStorage.getItem('patio2'));
 		carros.push(carro);
 		localStorage.setItem('patio2', JSON.stringify(carros));
 	}
 	
+	mostraPatio();
 
 	e.preventDefault();
+}
+
+function mostraPatio() {
+	var carros = JSON.parse(localStorage.getItem('patio2'));
+	var carrosResultado = document.getElementById('resultados');
+
+	carrosResultado.innerHTML = '';
+
+	for (var i = 0; i < carros.length; i++) {
+		var modelo = carros[i].modelo;
+		var placa = carros[i].placa;
+		var hora = carros[i].hora;
+		var minutos = carros[i].minutos;
+
+		carrosResultado.innerHTML += '<tr><td>' + modelo +
+								'</td><td>' + placa +
+								'</td><td>' + hora + ' : ' + minutos +
+								'</tr>';
+	}
 }
